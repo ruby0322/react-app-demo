b4da9b30b9964577cc25e4770c355ce0489be65c
Ruby <ruby0322@ntu.im>
2022-08-09 22:11:58 +0800

    ,

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Button from './Button'
 import React, { useEffect, useState } from 'react';
 
 const PROFILE_IAMGE_URL = 'https://media.discordapp.net/attachments/893439505988743178/1006503247349043230/08A95E27-9391-49C9-9079-227A4E16B7E5.JPG?width=801&height=1001';
+const PIG_IMAGE_URL = 'https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1644655550962.jpg';
 
 class Comment {
-  constructor(imgUrl, title, body, likeCnt, bg, avbg, titleColor, bodyColor) {
+  constructor(imgUrl, title, body, likeCnt, bg, avbg, titleColor, bodyColor, commentID, name) {
     this.imgUrl = imgUrl;
     this.title = title;
     this.body = body;
@@ -15,21 +16,55 @@ class Comment {
     this.avbg = avbg;
     this.titleColor = titleColor;
     this.bodyColor = bodyColor;
+    this.commentID = commentID;
+    this.name = name;
+    const current = new Date();
+    this.timestamp = current.toLocaleString();;
   }
 };
 
-const FIRST_COMMENT = new Comment(PROFILE_IAMGE_URL, 'POV：你點進來就只為了看我', '哈哈，好笨喔！對不起不要告我', 0, 'rgba(28, 32, 46, 0.88)', 'rgba(135,206,235, 0.15)', 'white', '#768CB1');
+const FIRST_COMMENT = new Comment(PROFILE_IAMGE_URL, 'POV：你點進來就只為了看我', '哈哈，好笨喔！對不起不要告我', 86, 'rgba(28, 32, 46, 0.88)', 'rgba(135,206,235, 0.15)', 'white', '#768CB1', 0, '顧寬証');
 
 const App = () => {
   const [comments, setComments] = useState([FIRST_COMMENT]);
+  const [titleInput, setTitleInput] = useState('');
+  const [bodyInput, setBodyInput] = useState('');
+  const [urlInput, setUrlInput] = useState('http://n.sinaimg.cn/sinakd10116/300/w1000h900/20200512/978a-itmiwrz3679873.jpg');
+  const newComment = () => {
+    setComments([...comments, 
+      new Comment(
+        urlInput || PIG_IMAGE_URL, 
+        titleInput || '我不會打字', 
+        bodyInput || '大家好，我是匿名笨豬，我超笨！', 
+        Math.floor(Math.random() * 2000), 
+        'rgba(28, 32, 46, 0.88)', 
+        'rgba(135,206,235, 0.15)', 
+        'white', 
+        '#768CB1', 
+        comments.length, 
+        '匿名笨豬'
+      )
+    ]);
+  };
   return (
-    <div id="main">
-      { comments.map(comment => <Panel {...comment} />) }
-      <Button 
-        text='+'
-        bg='rgba(28, 32, 46, 0.88)'
-        onClick={() => {setComments([...comments, FIRST_COMMENT])}}
-      />
+    <div id='main'>
+      <div id='scroll-container'>
+        <div id='comments'>
+          { comments.map(comment => <Panel {...comment} />) }
+        </div>
+      </div>
+      <div id='form'>
+        <div id='inputs'>
+          <input id='titleInput' className='input' placeHolder='標題' onChange={ evt => { setTitleInput(evt.target.value); }}></input>
+          <input id='bodyInput' className='input' placeHolder='內文'  onChange={ evt => { setBodyInput(evt.target.value); }}></input>
+          <input id='urlInput' className='input' placeHolder='頭貼超連結'  onChange={ evt => { setUrlInput(evt.target.value); }}></input>
+        </div>
+        <Button 
+          text='+'
+          bg='rgba(28, 32, 46, 0.88)'
+          onClick={newComment}
+        />
+      </div>
     </div>
   );
 };
--- src/Panel.js
+++ src/Panel.js
@@ -1,11 +1,6 @@
 import './Panel.css'
 import Hint from './Hint'
 import React, { useEffect, useState } from 'react';
-import { clear } from '@testing-library/user-event/dist/clear';
-
-const PROFILE_IAMGE_URL1 = 'https://media.discordapp.net/attachments/893439505988743178/1006500014933155840/IMG_8581-removebg-preview.png';
-const PROFILE_IAMGE_URL2 = 'https://media.discordapp.net/attachments/893439505988743178/1006501183462064209/E29BBEC2-038E-4C8D-8ED6-4201DFA43A85-removebg-preview.png';
-const PROFILE_IAMGE_URL3 = 'https://media.discordapp.net/attachments/893439505988743178/1006503247349043230/08A95E27-9391-49C9-9079-227A4E16B7E5.JPG?width=801&height=1001';
 
 const Panel = props => {
     const [liked, setLiked] = useState(false);
@@ -15,10 +10,10 @@ const Panel = props => {
         setLiked(!liked);
     };
     useEffect(() => {
-        if (likeCnt < 86) {
+        if (likeCnt < props.likeCnt) {
             let incrementLike = setInterval(() => {
                 setLikeCnt(likeCnt+1);
-            }, 50);
+            }, 20);
             return () => clearInterval(incrementLike);
         }
     }, [likeCnt]);
@@ -28,22 +23,32 @@ const Panel = props => {
             width: props.w || 'fit-content', 
             backgroundColor: props.bg
         }}>
-            <div className='avatar' style={{backgroundColor: props.avbg, backgroundImage: `url(${PROFILE_IAMGE_URL3})`, backgroundPositionY: '-8px', backgroundSize: 'cover'}}>
-                <Hint text='James Ku' w='100%' h='100%'/>
-            </div>
-            <div class="content">
-                <p className="title" style={{color: props.titleColor}}>
-                    {props.title}
-                </p>
-                <p className="body" style={{color: props.bodyColor}}>
-                    {props.body}
-                </p>
-                <p className="sign" 
-                    style={{color: '#ff8080'}}
-                    onClick={like} 
-                >
-                    {liked ? '♥' : '♡'} {likeCnt}
-                </p>
+            <div className='wrapper'>
+                <div className='avatar' style={{backgroundColor: props.avbg, backgroundImage: `url(${props.imgUrl})`, backgroundSize: 'cover'}}>
+                    <Hint text={props.name} w='100%' h='100%'/>
+                </div>
+                <div class="content">
+                    <p className="title" style={{color: props.titleColor}}>
+                        {props.title}
+                    </p>
+                    <p className="body" style={{color: props.bodyColor}}>
+                        {props.body}
+                    </p>
+                    <div className='footer'>
+                        <p className='comment-id'>
+                            #{props.commentID}
+                        </p>
+                        <p className='timestamp'>
+                            {props.timestamp}
+                        </p>
+                        <p className="sign" 
+                            style={{color: '#ff8080'}}
+                            onClick={like} 
+                            >
+                            {liked ? '♥' : '♡'} {likeCnt}
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
