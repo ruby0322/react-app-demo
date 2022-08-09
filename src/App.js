1ec7fe498d251382a80e881ceca0a9ba4f676a45
Ruby <ruby0322@ntu.im>
2022-08-09 23:36:23 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Panel from './Panel'
 import Button from './Button'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const PROFILE_IAMGE_URL = 'https://media.discordapp.net/attachments/893439505988743178/1006503247349043230/08A95E27-9391-49C9-9079-227A4E16B7E5.JPG?width=801&height=1001';
-const PIG_IMAGE_URL = 'https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1644655550962.jpg';
+const PIG_IMAGE_URL = 'https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1604418323329.jpg';
 
 class Comment {
   constructor(imgUrl, title, body, likeCnt, bg, avbg, titleColor, bodyColor, commentID, name) {
@@ -23,29 +23,41 @@ class Comment {
   }
 };
 
-const FIRST_COMMENT = new Comment(PROFILE_IAMGE_URL, 'POV：你點進來就只為了看我', '哈哈，好笨喔！對不起不要告我', 86, 'rgba(28, 32, 46, 0.88)', 'rgba(135,206,235, 0.15)', 'white', '#768CB1', 0, '顧寬証');
+const FIRST_COMMENT = new Comment(PROFILE_IAMGE_URL, 'POV：你點進來就只為了看我', '哈哈，好笨喔！', 87, 'rgba(28, 32, 46, 0.88)', 'rgba(135,206,235, 0.15)', 'white', '#768CB1', 0, '顧寬証');
+const SECOND_COMMENT = new Comment(PROFILE_IAMGE_URL, '不要告我', '好拉對不起，開玩笑的，不要告我拜託', 69, 'rgba(28, 32, 46, 0.88)', 'rgba(135,206,235, 0.15)', 'white', '#768CB1', 1, '顧寬証');
+const THIRD_COMMENT =  new Comment(PIG_IMAGE_URL, '嗨，那個長得笨笨的人類！', '對，就是你，新來的，說些話吧！', 520, 'rgba(28, 32, 46, 0.88)', 'rgba(135,206,235, 0.15)', 'white', '#768CB1', 2, '可ㄞㄉ小豬');
 
 const App = () => {
-  const [comments, setComments] = useState([FIRST_COMMENT]);
+  const [comments, setComments] = useState([]);
+  const commentsRef = useRef(comments);
+  commentsRef.current = comments;
   const [titleInput, setTitleInput] = useState('');
   const [bodyInput, setBodyInput] = useState('');
   const [urlInput, setUrlInput] = useState('http://n.sinaimg.cn/sinakd10116/300/w1000h900/20200512/978a-itmiwrz3679873.jpg');
-  const newComment = () => {
-    setComments([...comments, 
-      new Comment(
-        urlInput || PIG_IMAGE_URL, 
-        titleInput || '我不會打字', 
-        bodyInput || '大家好，我是匿名笨豬，我超笨！', 
-        Math.floor(Math.random() * 2000), 
-        'rgba(28, 32, 46, 0.88)', 
-        'rgba(135,206,235, 0.15)', 
-        'white', 
-        '#768CB1', 
-        comments.length, 
-        '匿名笨豬'
-      )
-    ]);
+  const getCommentInput = () => new Comment(
+    urlInput || PIG_IMAGE_URL, 
+    titleInput || '我不會打字', 
+    bodyInput || '大家好，我是匿名笨豬，我超笨！', 
+    Math.floor(Math.random() * 2000), 
+    'rgba(28, 32, 46, 0.88)', 
+    'rgba(135,206,235, 0.15)', 
+    'white', 
+    '#768CB1', 
+    comments.length, 
+    '匿名笨豬'
+  );
+  const newComment = comment => {
+    setComments([...commentsRef.current, comment]);
   };
+
+  useEffect(() => {
+    Howler.volume(1.0);
+    const timer1 = setTimeout(() => {newComment(FIRST_COMMENT)}, 1000);
+    const timer2 = setTimeout(() => {newComment(SECOND_COMMENT)}, 3500);
+    const timer3 = setTimeout(() => {newComment(THIRD_COMMENT)}, 5000);
+    return () => { clearTimeout(timer1); clearTimeout(timer2); clearTimeout(timer3); }
+  }, []);
+
   return (
     <div id='main'>
       <div id='scroll-container'>
@@ -62,7 +74,7 @@ const App = () => {
         <Button 
           text='💬'
           bg='rgba(28, 32, 46, 0.88)'
-          onClick={newComment}
+          onClick={() => { newComment(getCommentInput()); }}
         />
       </div>
     </div>
