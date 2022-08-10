430213fee255cdcbcfd99d4c5403ce48b5f48603
Ruby <ruby0322@ntu.im>
2022-08-10 18:25:04 +0800

    .

--- src/Playground.js
+++ src/Playground.js
@@ -2,7 +2,6 @@ import './App.css';
 import Panel from './Panel'
 import Button from './Button'
 import React, { useEffect, useState, useRef } from 'react';
-import FacebookLogin from 'react-facebook-login';
 
 const responseFacebook = (response) => {
     console.log(response);
@@ -11,14 +10,6 @@ const responseFacebook = (response) => {
 const Playground = props => {
     return (
         <div>
-            <FacebookLogin
-                appId="564191048775413"
-                autoLoad={true}
-                fields="name,email,picture"
-                callback={responseFacebook}
-                cssClass='facebook-login hover-scale'
-                icon="fa-facebook" 
-            />
         </div>
     );
 };
